d200adf87cd2517d4e850524bc83b09ee142ae09
ztx <737320730@qq.com>
2021-01-08 11:01:51 +0800

    完成了主页开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -18,18 +18,5 @@ $.ajaxPrefilter(function(options) {
     }
 
 
-    // 全局统一挂载 complete回调函数
-    options.complete = function(res) {
-        // console.log('执行了complete回调');
-        // console.log(res);
-        // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
-        // 如果请求结束，并且没有拿到认证失败
-        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
-            //1. 强制清空token
-            localStorage.removeItem('token');
-            // 2.强制跳转到登陆页
-            location.href = 'login.html';
-        };
 
-    }
 });
